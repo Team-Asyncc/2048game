550efb521caddca8c1281631b20b4690e7a3d664
ashirbad <ashirbadbehera29@gmail.com>
2021-08-02 00:05:44 +0530

    💄 add colors to cell

--- src/Components/Cell.js
+++ src/Components/Cell.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import '../styles/cell.scss';
 
+import { getColors } from '../utils/getColor';
+
 const Cell = ({ num }) => {
-	return <div className="cell">{num ? num : ''}</div>;
+	return (
+		<div style={{ backgroundColor: getColors(num) }} className="cell">
+			{num ? num : ''}
+		</div>
+	);
 };
 
 export default Cell;
