e3a0904a45009b0039a65c5385f6d450588deaf0
ayush-pandey007 <70948759+ayush-pandey007@users.noreply.github.com>
2021-08-01 20:08:17 +0530

    🐟 add initial state methods

--- src/Components/Cell.js
+++ src/Components/Cell.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../styles/cell.scss';
 
 const Cell = ({ num }) => {
-	return <div className="cell">{num}</div>;
+	return <div className="cell">{num ? num : ''}</div>;
 };
 
 export default Cell;
